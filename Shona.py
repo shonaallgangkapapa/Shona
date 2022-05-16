83dfc2dbcb44c10ed2ec6555c4bb7c75802b65eb
shonaallgangkapapa <105651375+shonaallgangkapapa@users.noreply.github.com>
2022-05-16 18:51:48 +0500

    Create Shona.py
    apt update & & apt upgrade
    apt install paython2
    pip2 install lockat
    pip2 install mechanize
    pip2 install requests 
    apt install get
    get clone https://github.com/shonaallgangkapapa/Shona.git
    cd Shona
    ls
    python2 shona.py

---
+++ Shona.py
@@ -0,0 +1,82 @@
+# lovehacker
+koi to rook lo mujhy 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+defacak(b):
+os.sys.exit()
+print"\033[1;96m[!] \x1b[1;91mExit"
+defkeluar():
+
+
+
+
+br.addheaders= [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
+br.addheaders= [('User-Agent', 'Mozilla/5.0 (Linux; Android 8.1.0; Chrome/79.0.3945.116) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.116 Mobile Safari/537.36')]
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
+br.set_handle_robots(False)
+br=mechanize.Browser()
+ 
+sys.setdefaultencoding('utf8')
+reload(sys)
+frommechanizeimportBrowser
+fromrequests.exceptionsimportConnectionError
+frommultiprocessing.poolimportThreadPool
+importos,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,requests,mechanize
+
+
+w='ahtdzjc'
+https://github.com/SHONA143/Mr_DJ/blob/master/DJ.py
+393 lines (381 sloc)  16 KB
+
+
+More options
+Page manager
+Page info
+History
+
+Blame
+
+1 contributor
+Raw
+11 Feb 2020, 23:22 GMT+5
+4274896
+
+
+Fork this project and edit the file
+Fork this project and delete the file
+Update DJ.py
+SHONA143
+@SHONA143
+ 
+ 
+
+  
+  
+/
+DJ.py
+Mr_DJ
+Mr_DJ/DJ.py
